3e19059e531d367061e5ce4b516465439f07e432
Louis Tur <louis.tur@gmail.com>
2016-06-28 19:37:31 -0400

    commit for travel

--- TextPlayground.playground/Contents.swift
+++ TextPlayground.playground/Contents.swift
@@ -6,22 +6,21 @@ var textStorage: NSTextStorage = NSTextStorage(string: "This is the Test String.
 var layoutManager: NSLayoutManager = NSLayoutManager()
 var arialTextContainer: NSTextContainer = NSTextContainer(size: CGSizeMake(200.0, 50.0))
 var menloTextContainer: NSTextContainer = NSTextContainer(size: CGSizeMake(200.0, 50.0))
-
+menloTextContainer.lineBreakMode = .ByCharWrapping
+menloTextContainer.lineFragmentPadding = 1.0
 textStorage.addLayoutManager(layoutManager)
 
 layoutManager.addTextContainer(arialTextContainer)
 layoutManager.addTextContainer(menloTextContainer)
 
 var testArialTextView: UITextView = UITextView(frame: CGRectMake(0,0,200,50), textContainer: arialTextContainer)
-
-
 var testMenloTextView: UITextView = UITextView(frame: CGRectMake(200, 50, 200, 50), textContainer: menloTextContainer)
-testMenloTextView
 
 var testView = UIView(frame: CGRectMake(0,0,420,120))
 testView.addSubview(testArialTextView)
 testView.addSubview(testMenloTextView)
 testView.backgroundColor = UIColor.redColor()
 
-
-testView
+let currentMenloBounds: CGRect = testMenloTextView.bounds
+let firstExclusionPath: UIBezierPath = UIBezierPath(ovalInRect: CGRectMake(100, 10, 10, 25))
+menloTextContainer.exclusionPaths = [firstExclusionPath]
